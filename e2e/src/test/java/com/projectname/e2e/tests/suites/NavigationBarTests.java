0063d6ee8a3b8842eb35dfb0da8b1ed0aa62ba62
Nikola Aleksic <nikola.z.aleksic@htecgroup.com>
2022-12-06 12:27:25 +0100

    Added homework changes

--- e2e/src/test/java/com/projectname/e2e/tests/suites/NavigationBarTests.java
+++ e2e/src/test/java/com/projectname/e2e/tests/suites/NavigationBarTests.java
@@ -1,8 +1,6 @@
 package com.projectname.e2e.tests.suites;
 
-import com.projectname.e2e.tests.pages.DemoPage;
-import com.projectname.e2e.tests.pages.FlightsPage;
-import com.projectname.e2e.tests.pages.NavigationBarPage;
+import com.projectname.e2e.tests.pages.*;
 import com.projectname.e2e.tests.suites.common.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -25,5 +23,41 @@ public class NavigationBarTests extends TestBase {
         FlightsPage flightsPage = navigationBarPage.openFlightsPage();
 
         Assert.assertTrue(flightsPage.isDisplayed());
+    }
+
+    @Test
+    public void verifyCarsButtonIsWorking() {
+        NavigationBarPage navigationBarPage = new NavigationBarPage(getDriver(), "", "", "");
+
+        CarsPage carsPage = navigationBarPage.openCarsPage();
+
+        Assert.assertTrue(carsPage.isDisplayed());
+    }
+
+    @Test
+    public void verifyHotelsButtonIsWorking() {
+        NavigationBarPage navigationBarPage = new NavigationBarPage(getDriver(), "", "", "");
+
+        HotelsPage hotelsPage = navigationBarPage.openHotelsPage();
+
+        Assert.assertTrue(hotelsPage.isDisplayed());
+    }
+
+    @Test
+    public void verifyPricingButtonIsWorking() {
+        NavigationBarPage navigationBarPage = new NavigationBarPage(getDriver(), "", "", "");
+
+        PricingPage pricingPage = navigationBarPage.openPricingPage();
+
+        Assert.assertTrue(pricingPage.isDisplayed());
+    }
+
+    @Test
+    public void verifyYoutubeButtonIsWorking() {
+        NavigationBarPage navigationBarPage = new NavigationBarPage(getDriver(), "", "", "");
+
+        YoutubePage youtubePage = navigationBarPage.openYoutubePage();
+
+        Assert.assertTrue(youtubePage.isDisplayed());
     }
 }
